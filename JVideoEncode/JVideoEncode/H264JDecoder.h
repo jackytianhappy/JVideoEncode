e20560f4b921ecea2de4cf5c2094c59b03dbb27c
Jacky <1142899950@qq.com>
2017-04-09 16:55:26 +0800

    添加解码文件

---
+++ JVideoEncode/JVideoEncode/H264JDecoder.h
@@ -0,0 +1,13 @@
+//
+//  H264JDecoder.h
+//  JVideoEncode
+//
+//  Created by Jacky on 2017/4/9.
+//  Copyright © 2017年 jacky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface H264JDecoder : NSObject
+
+@end
---
+++ JVideoEncode/JVideoEncode/H264JDecoder.m
@@ -0,0 +1,13 @@
+//
+//  H264JDecoder.m
+//  JVideoEncode
+//
+//  Created by Jacky on 2017/4/9.
+//  Copyright © 2017年 jacky. All rights reserved.
+//
+
+#import "H264JDecoder.h"
+
+@implementation H264JDecoder
+
+@end
